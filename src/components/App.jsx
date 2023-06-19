ff8e84153f20e4e6fa12511df958f7761d4f6187
Kushnir2205 <kushnirroman22@gmail.com>
2023-06-19 21:16:10 +0300

    task1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { Profile } from './Profile/Profile.jsx';
+import { Statistics } from './Statistics/Statistics.jsx';
+import { FriendList } from './FriendList/FriendList.jsx';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory.jsx';
+
+import transactions from '../data/transactions.json';
+import friends from '../data/friends.json';
+import data from 'data/data.json';
+import user from 'data/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
+    </>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,33 @@
+import css from './Statistics.module.css'
+
+const randColor = () => {
+    return (
+      '#' +
+      Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, '0')
+        .toUpperCase()
+    );
+  };
+
+export const Statistics = ({ title, stats }) => {
+    return (
+      <section className={css.statistics}>
+        {title && <h2 className={css.title}>{title}</h2>}
+  
+        <ul className={css.statList}>
+          {stats.map(el => (
+            <li 
+            style={{
+                backgroundColor: randColor(),
+                width: `calc(100% / ${stats.length})`,
+              }}
+            className={css.item} key={el.id}>
+              <span className={css.label}>{el.label}</span>
+              <span className={css.percentage}>{el.percentage}</span>
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  };
